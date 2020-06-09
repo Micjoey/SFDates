60d0b49317bff6745a898bbdc43663167fbe0776
Macallan Savett <jsavett@gmail.com>
2020-06-08 17:03:09 -0700

    fiddle with the navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
 import SearchBarContainer from './search_bar_container';
-import LoginFormContainer from '../session_form/login_form_container'
+
 
 
 
@@ -22,21 +22,23 @@ class Navbar extends React.Component {
 
 
 
-    personalGreeting() {
+    loggedIn() {
         return(
         <div className="header-group">
             <div className="header-group-logged-in">
                 <Link to="/home" className="SfDates-title-logged-in" >SFDates</Link>
                 <Link to="/home" className="nav-bar-home-button" >Home</Link>
+                <Link to="/home" className="nav-bar-home-button" >Saved Dates</Link>
+                
                 {/* <Link to='/Shelf' className="nav-bar-mybooks-button">Date Number</Link> */}
                 <button>Date Number Placeholder</button>
                 <SearchBarContainer allDates={this.props.retrieveDates()}/>
                 <div className="dropdown">
                     <img src={images.account_image} className="dropdown-image-icon" alt="" />
+                    <disabled className="header-currentuser-name">Hi, {this.props.currentUser.username}!</disabled>
                     <div className="dropdown-content">
-                        <disabled className="header-currentuser-name">Hi, {this.props.currentUser.username}!</disabled>
                         <Link to="/home" className="nav-bar-dropdown-mybooks-button" >Home</Link>
-                        <Link to='/Shelf' className="nav-bar-dropdown-mybooks-button">Saved Dates</Link>
+                        <Link to='/Saved_Dates' className="nav-bar-dropdown-mybooks-button">Saved Dates</Link>
                         <Link to='/home' className="nav-bar-dropdown-mybooks-button" onClick={this.props.logout}>Log Out</Link>
                     </div>
                 </div>
@@ -45,22 +47,21 @@ class Navbar extends React.Component {
         )
     }
     
-    sessionLinks() {
+    nonLoggedIn() {
         return (
             <div className="header-group">
                 <div className="header-group-logged-in">
                     <Link to="/home" className="SfDates-title-logged-in" >SFDates</Link>
-                    <Link to="/home" className="nav-bar-home-button" >Home</Link>
+                    <Link to="/home" className="nav-bar-home-button" >Pick a Date</Link>
                     {/* <Link to='/Shelf' className="nav-bar-mybooks-button">Date Number</Link> */}
                     <button>Date Number Placeholder</button>
                     <SearchBarContainer allDates={this.props.retrieveDates()} />
                     <div className="dropdown">
                         <img src="https://img.icons8.com/fluent/48/000000/menu-2.png" className="dropdown-image-icon" alt="" />
                         <div className="dropdown-content">
-                            {/* <disabled className="header-currentuser-name">Hi, {this.props.currentUser.username}!</disabled>
                             <Link to="/home" className="nav-bar-dropdown-mybooks-button" >Home</Link>
-                            <Link to='/Shelf' className="nav-bar-dropdown-mybooks-button">Saved Dates</Link>
-                            <Link to='/home' className="nav-bar-dropdown-mybooks-button" onClick={this.props.logout}>Log Out</Link> */}
+                            <h1>Sign Up Placeholder</h1>
+                            <h1>Login Placeholder</h1>
                         </div>
                     </div>
                 </div>
@@ -71,9 +72,9 @@ class Navbar extends React.Component {
         
         render() {
             if (this.props.currentUser) {
-                return this.personalGreeting()
+                return this.loggedIn()
             } else {
-                return this.sessionLinks()
+                return this.nonLoggedIn()
             }
         }
     }
