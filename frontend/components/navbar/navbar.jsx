2d9439a2687089574bde52bce853750f98f4f56c
Macallan Savett <jsavett@gmail.com>
2020-06-09 09:57:26 -0700

    add the div sections for the splash page try to get the background top image to render

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -29,16 +29,14 @@ class Navbar extends React.Component {
                 <Link to="/home" className="SfDates-title-logged-in" >SFDates</Link>
                 <Link to="/home" className="nav-bar-home-button" >Home</Link>
                 <Link to="/home" className="nav-bar-home-button" >Saved Dates</Link>
-                
                 {/* <Link to='/Shelf' className="nav-bar-mybooks-button">Date Number</Link> */}
-                <button>Date Number Placeholder</button>
                 <SearchBarContainer allDates={this.props.retrieveDates()}/>
                 <div className="dropdown">
                     <img src={images.account_image} className="dropdown-image-icon" alt="" />
                     <disabled className="header-currentuser-name">Hi, {this.props.currentUser.username}!</disabled>
                     <div className="dropdown-content">
                         <Link to="/home" className="nav-bar-dropdown-mybooks-button" >Home</Link>
-                        <Link to='/Saved_Dates' className="nav-bar-dropdown-mybooks-button">Saved Dates</Link>
+                        {/* <Link to='/Saved_Dates' className="nav-bar-dropdown-mybooks-button">Saved Dates</Link> */}
                         <Link to='/home' className="nav-bar-dropdown-mybooks-button" onClick={this.props.logout}>Log Out</Link>
                     </div>
                 </div>
@@ -52,10 +50,10 @@ class Navbar extends React.Component {
             <div className="header-group">
                 <div className="header-group-logged-in">
                     <Link to="/home" className="SfDates-title-logged-in" >SFDates</Link>
-                    <Link to="/home" className="nav-bar-home-button" >Pick a Date</Link>
+                    {/* <Link to="/home" className="nav-bar-home-button" >Pick a Date</Link> */}
                     {/* <Link to='/Shelf' className="nav-bar-mybooks-button">Date Number</Link> */}
-                    <button>Date Number Placeholder</button>
-                    <SearchBarContainer allDates={this.props.retrieveDates()} />
+                    {/* <button>Date Number Placeholder</button> */}
+                    {/* <SearchBarContainer allDates={this.props.retrieveDates()} /> */}
                     <div className="dropdown">
                         <img src="https://img.icons8.com/fluent/48/000000/menu-2.png" className="dropdown-image-icon" alt="" />
                         <div className="dropdown-content">
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -11,7 +11,21 @@ class Splash extends React.Component {
     render() {
         return (
             <div className='splash-page'>
-                <h1>Hi This is a long text</h1>
+                <div className='background-img'>
+                    <div>
+                        {/* <img src={image_url('color-sf-bridge.jpg')} alt=""/> */}
+                    </div>
+                    <div className='filter-date-bar'>
+
+                    </div>
+                    <div className='random-date-button'>
+
+                    </div>
+                </div>
+                <div className='random-date-box'>
+                    <div className='random-date-information'></div>
+                    <div className='random-date-image'></div>
+                </div>
             </div>
         )
     }
