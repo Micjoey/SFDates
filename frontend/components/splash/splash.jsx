46c688852c343df4202ad4368e1499bb0aa9b71f
Macallan Savett <jsavett@gmail.com>
2020-06-10 10:43:50 -0700

    add will h idea

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -23,8 +23,30 @@ class Splash extends React.Component {
                     </div>
                 </div>
                 <div className='random-date-box'>
-                    <div className='random-date-information'></div>
-                    <div className='random-date-image'></div>
+                    <div className='random-date-information'>
+                        <ul>
+                            <h3>Title - Terra Linda Sleepy Hollow Hike</h3>
+                        </ul>
+                        <ul>
+                            <h4>Location - San Rafeal</h4>
+                        </ul>
+                        <ul>
+                            <p>Cost - $</p> 
+                        </ul>
+                        <ul>
+                            <p>Type of Date - Hike</p>
+                        </ul>
+                        <ul>
+                            <p>Date Number - #1</p>
+                        </ul>
+                        <ul>
+                            
+                        </ul>
+                        <p></p>
+                    </div>
+                    <div className='random-date-image'>
+                        <img src="" alt="Filler"/>
+                    </div>
                 </div>
             </div>
         )
