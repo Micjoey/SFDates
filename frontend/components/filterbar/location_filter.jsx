958b3e8c4650bc3bc1350727c73fbec5df02f604
Macallan Savett <jsavett@gmail.com>
2020-06-16 13:30:22 -0700

    fix width of dropdown menu for location

--- frontend/components/filterbar/location_filter.jsx
+++ frontend/components/filterbar/location_filter.jsx
@@ -17,7 +17,7 @@ class LocationFilter extends React.Component {
         return(
             <div className="dropdown-menu">
                 {uniqueLocations.map((dateNumber, i) => (
-                    <ul className="dropdown-menu-items" key={i}>
+                    <ul className="dropdown-menu-items dropdown-menu-items-location" key={i}>
                         <Link className="no-link" to={`/datelocation/${i + 1}`}>{dateNumber}</Link>
                     </ul>
                 ))}
