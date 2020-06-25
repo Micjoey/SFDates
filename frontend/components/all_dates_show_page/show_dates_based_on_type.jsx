6efbb1f58ee9942c119772d2ce7419373604169e
Macallan Savett <jsavett@gmail.com>
2020-06-25 13:09:10 -0700

    get filter bar to work correctly

--- frontend/components/all_dates_show_page/show_dates_based_on_type.jsx
+++ frontend/components/all_dates_show_page/show_dates_based_on_type.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import IterateOverDates from './all_dates'
+import IterateOverDates from './iterate_over_dates'
 import HeaderName from './changing_header_name';
-
+import dateFilter from './date_filter'
 
 export const RenderDates = ({match}) => {
     const [currentDateList, setCurrentDateList] = useState({})
@@ -14,44 +14,45 @@ export const RenderDates = ({match}) => {
         fetchDates()
     }, [checkedBox])
     const [checkedBox, setCheckedBox] = useState({
-        costFilter: false,
-        dateNumberFilter: false,
-        locationFilter: false,
-        dateTypeFilter: false
+        cost: false,
+        date_number: false,
+        location: false,
+        date_type: false
     })
     const costAmount = ['Low', 'Medium', 'Expensive', 'Very Expensive']
     const dateNumber = ['Date #1', 'Date #2', 'Date #3', 'Date #4 or More']
-    
-    
     return (
         <div className='background-color'>
             <div className="date-specific-header-container">
                 <HeaderName/>
+                <h2>{Object.keys(currentDateList).length} - Count</h2>
+                <h2>{Object.values(checkedBox)} - test for setCheckedBox</h2>
             </div>
             <div className="date-specific-parent-container">
                 <div className="date-specific-filter">
+                    <button onClick={() => dateFilter(currentDateList, checkedBox, setCheckedBox, setCurrentDateList)}> Filter By:</button>
                     <div className="specific-filter">
                         <p>Cost: </p>
                         <div>
-                            {dropDownMenu(costAmount, "cost-date-drop-down")}
+                            {dropDownMenu(costAmount, "cost-date-drop-down", "cost")}
                         </div>
                     </div>
                     <div className="specific-filter">
                         <p>Type: </p>
                         <div>
-                            {dropDownMenu(allTypesOfDates(currentDateList),"type-date-drop-down")}
+                            {dropDownMenu(allTypesOfDates(currentDateList),"type-date-drop-down", "date_type")}
                         </div>
                     </div>
                     <div className="specific-filter">
                         <p>Location: </p>
                         <div>
-                            {dropDownMenu([],"type-date-drop-down")}
+                            {dropDownMenu([],"location-date-drop-down", "location")}
                         </div>
                     </div>
                     <div className="specific-filter">
                         <p>Date Number: </p>
                         <div>
-                            {dropDownMenu(dateNumber,"type-date-drop-down")}
+                            {dropDownMenu(dateNumber,"datenumber-date-drop-down", "date_number")}
                         </div>
                     </div>
                 </div>
@@ -71,20 +72,15 @@ export const RenderDates = ({match}) => {
 }
 
 
-const costFilter = (filteredDates) => {
 
-}
 
-const dateNumberFilter = (filteredDates) => {
 
-}
 
-const locationFilter = (filteredDates) => {
 
-}
-
-const dateTypeFilter = (filteredDates) => {
-
+const allTypesOfDates = (allDates) => {
+    const dates = Object.values(allDates)
+    const dateType = [...new Set(dates.map(date => date.date_type))]
+    return dateType
 }
 
 const dropDown = (className) => {
@@ -96,40 +92,17 @@ const dropDown = (className) => {
     }
 }
 
-const dropDownMenu = (menu, id) => {
-    // if (menu.length > 5) {
-    //     return (
-    //         <div id={id} className="date-drop-down">
-    //             <ul>
-    //                 {menu.map((item, idx) => (
-    //                     <li key={`${item} - ${idx}`}>
-    //                         <input id={`c${idx + 1}`} type="checkbox" />
-    //                         <label for={`c${idx + 1}`}>{item}</label>
-    //                     </li>
-    //                 ))}
-    //             </ul>
-    //         </div>
-    //     )
-    // } else {
-        return (
-           
-                <div id={id} className="date-drop-down">
-                    <ul>
-                        {menu.map((item, idx) => (
-                            <li key={`${item} - ${idx}`}>
-                                <input id={`c${idx + 1}`} type="checkbox" />
-                                <label for={`c${idx + 1}`}>{item}</label>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-        )
-    // }
-}
-
-const allTypesOfDates = (allDates) => {
-    const dates = Object.values(allDates)
-    const dateType = [...new Set(dates.map(date => date.date_type))]
-    // console.log(dateType)
-    return dateType
+const dropDownMenu = (menu, id, type = "default") => {
+    return (
+        <div id={id} className="date-drop-down">
+            <ul>
+                {menu.map((item, idx) => (
+                    <li key={`${item} - ${idx}`}>
+                        <input id={`${type}${idx + 1}`} type="checkbox" name={type} value={`${item}`} />
+                        <label for={`${type}${idx + 1}`}>{item}</label>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
