5733d22e0f49112bf34a62c3b16be69ba9aab91f
Macallan Savett <jsavett@gmail.com>
2020-06-26 14:03:31 -0700

    solved bug where the dates wouldnt filter correctly

--- frontend/components/all_dates_show_page/date_filter.jsx
+++ frontend/components/all_dates_show_page/date_filter.jsx
@@ -1,19 +1,20 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const dateFilter = (allDates, checkedBoxes, setCheckedBox, setCurrentDateList, dateType = ["cost", "date_type", "date_number", "location"]) => {
-    const originalDates = allDates
+const dateFilter = (allDates, originalDates, checkedBoxes, setCheckedBox, setCurrentDateList, dateType = ["cost", "date_type", "date_number", "location"]) =>   { 
     const originalDatesArray = Object.values(originalDates)
     let dateList = Object.values(allDates)
     let dateListHash = {}
-    let values = setValues(dateType, checkedBoxes)
-    checkedBoxes = changeCheckBoxes(checkedBoxes, values, setCheckedBox)
+    let values = setValues(dateType)
+    checkedBoxes = changeCheckBoxes(checkedBoxes, values, setCheckedBox)    
     dateList = setFilteredDates(values, originalDatesArray)
+    console.log(dateList)
     for (let x = 1; x <= dateList.length; x++) {
         dateListHash[x] = dateList[x - 1]
     }
-    debugger
     setCurrentDateList(dateListHash)
+
+
 }
 
 const changeCheckBoxes = (checkedBoxes, newCheckedBoxes, setCheckedBoxes) => {
@@ -26,37 +27,35 @@ const changeCheckBoxes = (checkedBoxes, newCheckedBoxes, setCheckedBoxes) => {
 
 const setValues = (dateType) => {
     let values = {}
+    let count = 0;
     dateType.forEach(type => {
         const checkbox = document.querySelectorAll(`input[name=${type}]:checked`)
+        count += checkbox.length
         if (checkbox.length > 0) {
             values[type] = []
             checkbox.forEach((checkbox) => {
                 values[type].push(checkbox.value)
+
             })
         }
     })
     return values
 }
 
-const setFilteredDates = (values, dateList) => {
-    dateList = dateList.slice('')
-    let newDateListArray = []
-    let keys
-    if (values) {
-        keys = Object.keys(values)
+const setFilteredDates = (values = {}, dateList) => {
+    let newDateListArray = Object.values(dateList)
+    let keys = Object.keys(values)
+    if (keys.length) {
         keys.forEach(key => {
             let toFilterParts = values[key]
+            dateList = newDateListArray
             for (let x = 0; x < toFilterParts.length; x++) {
                 let filterPiece = toFilterParts[x]
-                dateList.map(date => {
-                    if (date[key] === filterPiece)
-                        newDateListArray.push(date)
-                    })
-                    // newDateList.concat(tempArray)
-                }
-            })
-        }
-    return newDateListArray
+                newDateListArray = dateList.filter(date => date[key] === filterPiece)
+            }
+        })
+    }
+    return [...new Set(newDateListArray)]
 }
 
 export default dateFilter
--- frontend/components/all_dates_show_page/show_dates_based_on_type.jsx
+++ frontend/components/all_dates_show_page/show_dates_based_on_type.jsx
@@ -18,6 +18,7 @@ export const RenderDates = ({match}) => {
         }
         fetchDates()
     }, [checkedBox])
+    
     const [checkedBox, setCheckedBox] = useState({
         cost: false,
         date_number: false,
@@ -33,7 +34,7 @@ export const RenderDates = ({match}) => {
                 </div>
                 <div className="date-specific-parent-container">
                     <div className="date-specific-filter">
-                        <button onClick={() => dateFilter(originalDateList, checkedBox, setCheckedBox, setCurrentDateList)}> Filter By:</button>
+                        <button onClick={() => dateFilter(currentDateList, originalDateList, checkedBox, setCheckedBox, setCurrentDateList)}> Filter By:</button>
                         <button onClick={() => resetFilter(originalDateList, setCurrentDateList)}> Reset Search:</button>
                         <div className="specific-filter">
                             <p>Cost: </p>
@@ -91,6 +92,7 @@ const resetFilter = (originalDateList, setCurrentDateList) => {
 const grabUniqAspectOfDate = (allDates, uniqAspect) => {
     const dates = Object.values(allDates)
     const dateType = [...new Set(dates.map(date => date[uniqAspect]))]
+    
     switch (uniqAspect) {
         case "date_type":
             return dateType.sort()
