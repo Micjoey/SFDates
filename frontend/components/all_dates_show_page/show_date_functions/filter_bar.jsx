b39741bcf4aab4854441c444914a09f86d346adb
Macallan Savett <jsavett@gmail.com>
2020-07-09 10:49:39 -0700

    refactor filters to add in filterBar function to clean it up

---
+++ frontend/components/all_dates_show_page/show_date_functions/filter_bar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import dropDownMenu from './drop_down_menu'
+import dropDown from './drop_down'
+const filterBar = (filter, typeOfDropDown, filterTypeDropDown, uniqList, 
+                    currentDateList, originalDateList,checkedBox,
+                    setCheckedBox, setCurrentDateList, filterForDate,setFiltered) => {
+            
+                    if (filter === "Type: " || filter === "Location: ") {
+                        debugger
+                        return (
+                            <div className="specific-filter">
+                                <p>{filter}</p>
+                                <div className="date-type-filter">
+                                    {dropDownMenu(
+                                        uniqList,
+                                        typeOfDropDown,
+                                        filterTypeDropDown,
+                                        currentDateList,
+                                        originalDateList,
+                                        checkedBox,
+                                        setCheckedBox,
+                                        setCurrentDateList,
+                                        filterForDate,
+                                        setFiltered)}
+
+                                    <div
+                                        onClick={() => dropDown(typeOfDropDown)}
+                                        className="see-more-button"
+                                    >
+                                        <i className="fa fa-angle-double-down down-arrow"> </i>
+                                                    See more
+                                        <i className="fa fa-angle-double-down down-arrow"> </i>
+                                    </div>
+
+                                </div>
+                            </div>
+                        )
+                    } else {
+                        return (
+                            <div className="specific-filter">
+                                <p>{filter}</p>
+                                <div>
+                                    {dropDownMenu(uniqList, typeOfDropDown, filterTypeDropDown,
+                                                currentDateList, originalDateList, checkedBox,
+                                                setCheckedBox, setCurrentDateList, filterForDate,
+                                                setFiltered)}
+                                </div>
+                            </div>
+                        )
+                    }
+                }
+
+export default filterBar
+                
--- frontend/components/all_dates_show_page/show_dates_based_on_type.jsx
+++ frontend/components/all_dates_show_page/show_dates_based_on_type.jsx
@@ -7,6 +7,7 @@ import grabUniqAspectOfDate from './show_date_functions/grab_uniq_aspects_of_dat
 import dropDownMenu from './show_date_functions/drop_down_menu';
 import resetFilter from './show_date_functions/reset_filter';
 import thirdParty from './show_date_functions/is_third_party';
+import filterBar from './show_date_functions/filter_bar';
 
 
 
@@ -61,80 +62,19 @@ export const RenderDates = ({match}) => {
                             Filter By:
                         </button>
                         <div className="date-specific-filter-container">
-                            <div className="specific-filter">
-                                <p>Type: </p>
-                                <div className="date-type-filter">
-                                    {dropDownMenu(
-                                        dateTypeUniqList,
-                                         "date-type-drop-down",
-                                        "date_type",
-                                        currentDateList, 
-                                        originalDateList, 
-                                        checkedBox, 
-                                        setCheckedBox, 
-                                        setCurrentDateList,
-                                        filterForDate,
-                                        setFiltered)}
-                                    
-                                    <div
-                                        onClick={() => dropDown("date-type-drop-down")}
-                                        className="see-more-button"
-                                    >
-                                        <i className="fa fa-angle-double-down down-arrow"> </i>
-                                            See more
-                                        <i className="fa fa-angle-double-down down-arrow"> </i>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                            <div className="specific-filter">
-                                <p>Date Number: </p>
-                                <div>
-                                    {dropDownMenu(dateNumberUniqList, "datenumber-date-drop-down", "date_number", 
-                                        currentDateList,
-                                        originalDateList,
-                                        checkedBox,
-                                        setCheckedBox,
-                                        setCurrentDateList,
-                                        filterForDate,
-                                        setFiltered)}
-                                </div>
-                            </div>
-                            <div className="specific-filter">
-                                <p>Cost: </p>
-                                <div>
-                                    {dropDownMenu(costUniqList, "cost-date-drop-down", "cost", 
-                                        currentDateList,
-                                        originalDateList,
-                                        checkedBox,
-                                        setCheckedBox,
-                                        setCurrentDateList,
-                                        filterForDate,
-                                        setFiltered)}
-                                </div>
-                            </div>
-                            <div className="specific-filter">
-                                <p>Location: </p>
-                                <div className="location-filter">
-                                    {dropDownMenu(locationUniqList, "location-date-drop-down", "location", 
-                                        currentDateList,
-                                        originalDateList,
-                                        checkedBox,
-                                        setCheckedBox,
-                                        setCurrentDateList,
-                                        filterForDate,
-                                        setFiltered)}
-                                    <div
-                                        onClick={() => dropDown("location-date-drop-down")}
-                                        className="see-more-button"
-                                    >
-                                        <i className="fa fa-angle-double-down down-arrow"> </i>
-                                            See more
-                                        <i className="fa fa-angle-double-down down-arrow"> </i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            {filterBar("Type: ", "date-type-drop-down","date_type", dateTypeUniqList,
+                                        currentDateList, originalDateList, checkedBox,
+                                        setCheckedBox, setCurrentDateList, filterForDate, setFiltered)}
+                            {filterBar("Date Number: ", "datenumber-date-drop-down", "date_number", dateNumberUniqList,
+                                        currentDateList, originalDateList, checkedBox,
+                                        setCheckedBox, setCurrentDateList, filterForDate, setFiltered)}
+                            {filterBar("Cost: ", "cost-date-drop-down", "cost", costUniqList,
+                                        currentDateList, originalDateList, checkedBox,
+                                        setCheckedBox, setCurrentDateList, filterForDate, setFiltered)}
+                            {filterBar("Location: ", "location-date-drop-down", "location", locationUniqList,
+                                        currentDateList, originalDateList, checkedBox,
+                                        setCheckedBox, setCurrentDateList, filterForDate, setFiltered)}
+                        </div> 
                         <button onClick={() => resetFilter(originalDateList, setCurrentDateList)}> Reset Search:</button>
                     </div>
                     <div className="date-specific-info">
@@ -174,14 +114,7 @@ export const RenderDates = ({match}) => {
 
 
 
-const dropDown = (idName) => {
-    const currentDiv = document.getElementById(`${idName}`)
-    if (currentDiv.style.display === "none" || currentDiv.style.display === "") {
-        currentDiv.style.display = "block"
-    } else {
-        currentDiv.style.display = "none"
-    }
-}
+
 
 
 
