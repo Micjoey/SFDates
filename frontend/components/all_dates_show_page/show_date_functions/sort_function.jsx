a93bf7e61788e25c329f66625e012c3311644b62
Macallan Savett <jsavett@gmail.com>
2020-07-16 13:07:47 -0700

    make changes

---
+++ frontend/components/all_dates_show_page/show_date_functions/sort_function.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const sortingOptions = () => {
+    
+}
+
+const sortDropDown = (currentDateList, originalDateList) => {
+    
+    if (currentDateList.length) {
+
+    }
+}
