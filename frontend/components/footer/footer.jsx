497106c24e2cb4ecd96df6a839b9deb2aefec5a0
Macallan Savett <jsavett@gmail.com>
2020-10-04 14:37:07 -0700

    add tracker links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-
+// linkedin tracker: https://bit.ly/3ivEr6L
+// portfolio tracker: https://bit.ly/2F0FCgR
+// github tracker: https://bit.ly/2SoUmsL
 class Footer extends React.Component {
     render() {
         return (
             <div className="footer-inner">
-                <a href="https://www.linkedin.com/in/macallan-savett/" rel="noopener noreferrer" target="_blank" >
+                <a href="https://bit.ly/3ivEr6L" rel="noopener noreferrer" target="_blank" >
                     <img className="footer-linkedin" src={images.linkedin}/>
                 </a>
-                <a href="https://github.com/Micjoey/SFDates" rel="noopener noreferrer" target="_blank" >
+                <a href="https://bit.ly/2SoUmsL" rel="noopener noreferrer" target="_blank" >
                     <img src={images.github} className="footer-github" />
                 </a>
-                <a href="https://macallan.space" id="footerLink" rel="noopener noreferrer" target='_blank'>Personal Portfolio</a>
+                <a href="https://bit.ly/2F0FCgR" id="footerLink" rel="noopener noreferrer" target='_blank'>Personal Portfolio</a>
             </div>
         );
     }
