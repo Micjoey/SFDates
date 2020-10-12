5a79fc65d089fac9bca596be2393e0737523442e
Macallan Savett <jsavett@gmail.com>
2020-10-12 11:49:58 -0700

    "changes to footer"

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -13,11 +13,11 @@ class Footer extends React.Component {
                 <a href="https://bit.ly/2SoUmsL" rel="noopener noreferrer" target="_blank" >
                     <img src={images.github} className="footer-github" />
                 </a>
-                <a href="https://bit.ly/2F0FCgR" id="footerLink" rel="noopener noreferrer" target='_blank'>
-                    Personal Portfolio
-                </a>
                 <a href="https://bit.ly/2F0FCgR" rel="noopener noreferrer" target='_blank'>
-                    <img src={images.personal_logo} className="footer-logo" alt="personal-logo"/>
+                    <figure>
+                        <img src={images.personal_logo} className="footer-logo" alt="personal-logo"/>
+                        <figcaption className="logo-caption">Personal Portfolio</figcaption>
+                    </figure>
                 </a>
             </div>
         );
